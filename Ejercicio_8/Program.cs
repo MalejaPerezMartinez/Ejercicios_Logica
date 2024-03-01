11b04adb89bdbc4cc8f605169a434b70336435de
MARIA ALEJANDRA PEREZ MARTINEZ <malejaperezm9856@gmail.com>
2024-02-29 21:26:33 -0500

    Ejercicio8

---
+++ Ejercicio_8/Program.cs
@@ -0,0 +1,30 @@
+﻿internal class Program
+{
+    public static void Main(string[] args)
+    {
+        //Construir un programa que pida un número y luego diga si este número es par o no.
+        double n1;
+        // Solicitar el numero 
+        Console.Write("Ingrese un numero : ");
+        // Convertir numero
+        n1=Convert.ToDouble(Console.ReadLine());
+        // Validaciones
+
+        if(n1 == 0)
+        {
+            Console.Write("Es par");
+
+        }
+
+        else 
+        if (((n1/n1))==1)
+        {
+            Console.Write("Es par");
+        }
+         else
+        {
+            Console.Write("Es impar");
+        }
+
+    }
+}
